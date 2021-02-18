4b9cbfc6f963aab4eab43cbd3ea679c767c1c227
Moose <aymanm95@gmail.com>
2021-02-18 20:55:11 +0000

    fix: remove linty folder

--- linty/Compiler.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#ifdef __GNUC__
-    #define COMPILER_NAME "g++"
-    #define COMPILER_VERSION __VERSION__
-#else
-    #define COMPILER_NAME "Unknown compiler"
-    #define COMPILER_VERSION "Unknown compiler version"
-#endif
--- linty/Jenkins.h
+++
@@ -1,7 +0,0 @@
-#ifndef JENKINS_H 
-#define JENKINS_H 
-#define APPLICATION_NAME "Linty" 
-#define BUILD_VERSION "1.26" 
-#define BUILD_DATE "2020-08-24_22-46-16" 
-#define BUILD_COMMIT "1d21ebda018435a211b213550d0d619b0d68c962" 
-#endif // JENKINS_H 
